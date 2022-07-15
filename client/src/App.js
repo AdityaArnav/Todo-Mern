0c1b7006684115ecb3b0fc4b78b0e5c46d950d42
AdityaArnav <99977930+AdityaArnav@users.noreply.github.com>
2022-07-15 16:45:08 +0530

    todo completed

--- client/src/App.js
+++ client/src/App.js
@@ -7,13 +7,16 @@ function App() {
   // Adding new todo item to database
   const [itemText,setItemText] = useState('');
   const [listItems, setListItems] = useState([]);
+  const [isUpdating, setIsUpdating] = useState('');
+  const [updateItemText,setUpdateItemText] = useState('');
 
 
   const addItem = async (e)=>{
     e.preventDefault();
     try{
       const res = await axios.post('http://localhost:5500/api/item',{item:itemText})
-      console.log(res);
+      // console.log(res);
+      setListItems(prev =>[...prev,res.data]);
       setItemText('');
     }
     catch(err){
@@ -40,12 +43,38 @@ function App() {
   const deleteItem = async (id)=>{
     try{
       const res = await axios.delete(`http://localhost:5500/api/item/${id}`)
-      console.log(res.data);
+      const newListItems = listItems.filter(item => item._id !==id);
+      setListItems(newListItems)
     }
     catch(err){
       console.log(err);
     }
   }
+
+  // update item
+  const updateItem = async(e)=>{
+    e.preventDefault()
+    try{
+    const res = await axios.put(`http://localhost:5500/api/item/${isUpdating}`,{item:updateItemText})
+    console.log(res.data);
+    const updatedItemIndex = listItems.findIndex(item=>item._id===isUpdating);
+    const updateItem = listItems[updatedItemIndex].item = updateItemText;
+    setUpdateItemText('');
+    setIsUpdating('');
+
+    }
+    catch(err){
+      console.log(err);
+    }
+  }
+  // before updating item i need to show input field where i will create my update
+
+  const renderUpdateForm = ()=>(
+    <form className ='update-form' onSubmit={(e) => {updateItem(e)}}>
+      <input className='update-new-input' type='text' placeholder='New Item' onChange={e => {setUpdateItemText(e.target.value)}} value={updateItemText}/>
+      <button className='update-new-btn' type='submit'>Update</button>
+    </form>
+  )
   return (
     <div className="App">
     <h1>Todo List</h1>
@@ -58,9 +87,15 @@ function App() {
       listItems.map(item =>(
 
       <div className='todo-item'>
+      {
+        isUpdating === item._id
+        ? renderUpdateForm()
+        : <>
         <p className='item-content'>{item.item}</p>
-        <button className='update-item'>Update</button>
+        <button className='update-item'onClick={()=>{setIsUpdating(item._id)}}>Update</button>
         <button className='delete-item' onClick ={()=>{deleteItem(item._id)}}>Delete</button>
+        </>
+      }
       </div>
       ))
     }
